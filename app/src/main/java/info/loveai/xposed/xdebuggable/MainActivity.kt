61f646623b9b51caafbbe32ca4c326de8c0724ab
sundayliu <sundayliu@aliyun.com>
2019-04-13 18:10:24 +0800

    init project

---
+++ app/src/main/java/info/loveai/xposed/xdebuggable/MainActivity.kt
@@ -0,0 +1,12 @@
+package info.loveai.xposed.xdebuggable
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
